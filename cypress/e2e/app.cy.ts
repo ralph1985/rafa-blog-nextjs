22f56be838f68d5dc7aae9c08e297a5b95ee7d31
RAFAEL GARCÍA PRIETO <rafael.garcia.prieto@bbvaitspain.com>
2023-04-02 19:39:26 +0200

    feat: add cypress for tests e2e

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,18 @@
+// Cypress E2E Test
+describe('Navigation', () => {
+  it('should navigate to the home page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    // The new page should contain an h1 with "About page"
+    cy.get('p').contains('Get started by editing');
+
+    // Find a link with an href attribute containing "about" and click it
+    // cy.get('a[href*="about"]').click();
+    // The new url should include "/about"
+    // cy.url().should('include', '/about');
+  });
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {};
