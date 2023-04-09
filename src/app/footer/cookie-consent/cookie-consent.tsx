ffc15c7fb016c6e9812a19177e2e156d66a2f5d1
RAFAEL GARCÍA PRIETO <rafael.garcia.prieto@bbvaitspain.com>
2023-04-09 20:53:09 +0200

    feat: cookie consent improvements

--- src/app/footer/cookie-consent/cookie-consent.tsx
+++ src/app/footer/cookie-consent/cookie-consent.tsx
@@ -6,13 +6,15 @@ import styles from './cookie-consent.module.scss';
 
 export default function CookieConsent() {
   const { t } = useTranslation('common');
-  const [consentGiven, setConsentGiven] = useState<boolean>(false);
+  const [consentGiven, setConsentGiven] = useState<boolean>(true);
 
   useEffect(() => {
     const isConsentGiven = sessionStorage.getItem('cookieConsent'); // TODO: sessionStorage se tiene que hacer a través de un "servicio"
 
     if (isConsentGiven) {
       setConsentGiven(true);
+    } else {
+      setConsentGiven(false);
     }
   }, []);
 
@@ -22,15 +24,13 @@ export default function CookieConsent() {
   };
 
   return (
-    <div>
-      {!consentGiven && (
-        <div className={styles.style} data-testid="cookieConsent">
-          <p>{t('cookieConsent')}</p>
-          <button type="button" onClick={handleAccept}>
-            {t('accept')}
-          </button>
-        </div>
-      )}
-    </div>
+    !consentGiven && (
+      <div className={styles.style} data-testid="cookieConsent">
+        <p>{t('cookieConsent')}</p>
+        <button type="button" onClick={handleAccept}>
+          {t('accept')}
+        </button>
+      </div>
+    )
   );
 }
