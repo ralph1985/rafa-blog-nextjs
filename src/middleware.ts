bf4cd4f7cdf42394fc2790675cbad5fd52f806db
RAFAEL GARCÍA PRIETO <rafael.garcia.prieto@bbvaitspain.com>
2023-04-09 19:40:17 +0200

    Revert "fix: remove middleware temporary"
    This reverts commit c9ac6818248b922e314d25d588903adce8439385.

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import i18n from '../i18n';
+
+const middleware = (request: NextRequest) => {
+  const locale = request.nextUrl.locale || i18n.defaultLocale;
+  const regex = /\w+\.(svg|jpg|png)$/i;
+
+  if (!regex.test(request.nextUrl.href)) {
+    request.nextUrl.searchParams.set('lang', locale);
+    request.nextUrl.href = request.nextUrl.href.replace(`/${locale}`, '');
+  }
+
+  return NextResponse.rewrite(request.nextUrl);
+};
+
+export default middleware;
