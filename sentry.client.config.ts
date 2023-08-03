da0db5ee76d8cc9fd3231009b90e97e0265ac900
RAFAEL GARCÍA PRIETO <rafael.garcia.prieto@bbvaitspain.com>
2023-08-04 00:05:01 +0200

    feat: add sentry environment

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,6 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENV,
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
